370153c5d3d0231cf0ac7eae33ea13124585370f
Danilo Ventura <danilo.ventura@scai-consulting.grupposcai.it>
2021-10-28 10:58:00 +0200

    organized import

--- src/test/java/it/ventura/pound/pound/OperationResultTest.java
+++ src/test/java/it/ventura/pound/pound/OperationResultTest.java
@@ -1,6 +1,6 @@
 package it.ventura.pound.pound;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
